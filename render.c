0e5f8ae2700e7098cf6a77401122b7ed8f61c3b1
MarianoA18 <mariano.arce.n@gmail.com>
2026-07-05 22:09:20 -0600

    agrego metodos nuevos al render.c

--- render.c
+++ render.c
@@ -32,3 +32,37 @@ void render_serpiente(Juego *j) {
     for (int i = 0; i < j->longitud; i++)
         DrawRectangle((int)j->cuerpo[i].x, (int)j->cuerpo[i].y, 25, 25, (Color){0, 255, 255, 255});
 }
+
+// Dibuja la comida como un cuadrado rojo
+void render_comida(Juego *j) {
+    DrawRectangle((int)j->comida.x, (int)j->comida.y, 25, 25, RED);
+}
+
+// Dibuja el título "SNAKE" y el puntaje
+void render_hud(int puntaje) {
+    DrawText("SNAKE", 400, 20, 60, DARKBLUE);
+    DrawText("Puntuacion:", 50, 40, 22, BLACK);
+    DrawText(TextFormat("%d", puntaje), 185, 40, 22, BLACK);
+}
+
+// Pantalla de game over con el puntaje final
+void render_gameover(int puntaje) {
+    ClearBackground(RAYWHITE);
+    DrawText("GAME OVER", 700/2 - MeasureText("GAME OVER", 60)/2, 250, 60, RED);
+    DrawText(TextFormat("Puntaje: %d", puntaje),
+             700/2 - MeasureText(TextFormat("Puntaje: %d", puntaje), 28)/2,
+             330, 28, DARKGRAY);
+    DrawText("Presiona ENTER para volver al menu",
+             700/2 - MeasureText("Presiona ENTER para volver al menu", 18)/2,
+             390, 18, GRAY);
+}
+
+// Dibuja todo lo del juego de una vez
+void render_juego(Juego *j) {
+    ClearBackground(RAYWHITE);
+    render_cuadricula();
+    render_bordes();
+    render_comida(j);
+    render_serpiente(j);
+    render_hud(j->puntaje);
+}
